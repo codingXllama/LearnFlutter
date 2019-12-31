c3f1135e50fb9e8547a68390c35825a056d2bae5
codingXllama <osamaalchalabi2@gmail.com>
2019-12-31 02:50:52 -0500

    Created a basic scaffold layout with notes

--- Demo1/first_app/lib/main.dart
+++ Demo1/first_app/lib/main.dart
@@ -2,5 +2,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Text("First App"),
+  home:Scaffold(
+    // How our app looks on the top
+    //Widgets start with CapLetter
+    //Make sure to add ',' after each property 
+    appBar: AppBar(
+      title:Text("Hello"),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("This is the child text within the body"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child:Text('click'),
+    ),
+  ),
 ));
